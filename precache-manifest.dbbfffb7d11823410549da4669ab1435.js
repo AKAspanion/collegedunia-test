d855f096f236c1e18d13df58289d4de3c05aa3ef
Ankit Kumar <panditankit1995@gmail.com>
2020-07-05 12:39:55 +0530

    Updates

---
+++ precache-manifest.dbbfffb7d11823410549da4669ab1435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7b049d5552607cfc2becb9cd24615f",
+    "url": "/collegedunia-test/index.html"
+  },
+  {
+    "revision": "24b77328d1e55bef56f7",
+    "url": "/collegedunia-test/static/css/main.f61ff9fd.chunk.css"
+  },
+  {
+    "revision": "4d1aeaaeec8e0b48036a",
+    "url": "/collegedunia-test/static/js/2.05d7774e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegedunia-test/static/js/2.05d7774e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b77328d1e55bef56f7",
+    "url": "/collegedunia-test/static/js/main.fae0c2c6.chunk.js"
+  },
+  {
+    "revision": "fa50a5320bb50ab666e0",
+    "url": "/collegedunia-test/static/js/runtime-main.3a7f6668.js"
+  },
+  {
+    "revision": "771eb93f7a91b889be7e6d7773c84138",
+    "url": "/collegedunia-test/static/media/DINPro-Regular.771eb93f.woff"
+  }
+]);
